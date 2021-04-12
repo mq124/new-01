8a568dc8243b2521e61ac6b2bd986013cc959a83
mq <3447415627@qq.com>
2021-04-12 16:41:29 +0800

    提交首页

--- assets/lib/baseapi.js
+++ assets/lib/baseapi.js
@@ -1,4 +1,23 @@
 $.ajaxPrefilter(function(options) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
-  })
+  
+    if(options.url.includes('/my/')) {
+      options.headers = {
+        Authorization:localStorage.token || '',
+      }
+      options.complete = function(res) {
+        // const {responseJSON} = res
+        // const {status} = responseJSON;
+        // const {message} = responseJSON 
+        if(res.status === 1 && res.responseJSON.message === "身份认证失败！") {
+          
+            location.href = './login.html'
+         
+        }
+        
+      }
+    };
+
+    
+})
---
+++ js/ind1.js
@@ -0,0 +1,60 @@
+$(function(){
+    giruserinfo();
+    // 绑定退出点击事件
+    $('#quit').on('click', function(){
+       layer.confirm('确认退出登录吗？',
+       {icon:3,title:'提示'},
+       function(index) {
+            // console.log(index);
+            
+            localStorage.removeItem('token');
+            // 跳转到登录主页面
+            location.href = './login.html';
+
+            layer.close(index);
+       })
+    })
+});
+
+function giruserinfo() {
+    $.ajax({
+        type: "GET",
+        url:'/my/userinfo',
+       
+        success:function(res){
+            console.log(res);
+            if(res.status !== 0) {
+                return layui.layer.msg('获取用户失败')
+            }
+            renderAvtar(res.data);
+        },
+        // complete:function(res) {
+        //     console.log(res)
+        //     const {responseJSON} = res
+        //     const {status} = responseJSON;
+        //     const {message} = responseJSON 
+        //     if(status === 1 && 
+        //         message === '身份认证失败！') 
+        //     {
+        //        location.href = './login.html'
+        //     }
+        // }
+    });
+}
+
+ // 渲染用户头像
+function renderAvtar(user) {
+    // 获取用户名
+   const name = user.nickname || user.username
+   
+   $('.welcome1').html('欢迎' + name);
+   if(user.user_pic) {
+         $('#layui-nav-img').attr('src',user.user_pic).show();
+         $('.text-avatar').hide()
+   } else{
+        $('.layui-nav-img').hide();
+        const first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show
+   }
+
+}
